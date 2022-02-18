6bb37f47b83480b32cadbf0f85a474162b751326
FumiKobayashi <fkobayashi@qi.mp.es.osaka-u.ac.jp>
2022-02-18 17:05:20 +0900

    Modified setup.py to fix a bug that prevented the library from being installed correctly.

--- setup.py
+++ setup.py
@@ -17,13 +17,14 @@ extras_require={
     "qlib":["qulacs", "qiskit", "amazon-braket-sdk"]
 }
 
-py_modules=[splitext(basename(path))[0] for path in glob('converter/*.py')]
+quantum_packages = ['qulacs', 'qiskit', 'braket']
+py_modules=['naniwa.converter.{0}.{0}_converter'.format(q_package) for q_package in quantum_packages]
 
 setup(
     name='naniwa',
     version='0.0.1',
     description='This is a library of converting qulacs circuits to an quantum circuit on another library.',
-    packages=packages,
+    # packages=packages,
     install_requires=install_requires,
     py_modules=py_modules,
 )
