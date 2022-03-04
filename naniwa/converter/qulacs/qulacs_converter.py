998211c74c0307e4e13b0e4b60d140bb422673a1
rsobt <iida_t@eis.hokudai.ac.jp>
2022-03-04 16:24:44 +0900

    Merge qulacs_converter.py and braket_converter.py

--- naniwa/converter/qulacs/qulacs_converter.py
+++ naniwa/converter/qulacs/qulacs_converter.py
@@ -1,5 +1,6 @@
 import qulacs 
 import qiskit
+import braket
 
 qiskit_dict = {
     "I":          [qiskit.circuit.library.standard_gates.IGate,    0],
@@ -23,6 +24,25 @@ qiskit_dict = {
     "sqrtXdag":   [qiskit.circuit.library.standard_gates.SXdgGate, 0],
 }
 
+braket_dict = {
+    "I":           [braket.circuits.gates.I,       0],
+    "X":           [braket.circuits.gates.X,       0],
+    "Y":           [braket.circuits.gates.Y,       0],
+    "Z":           [braket.circuits.gates.Z,       0],
+    "H":           [braket.circuits.gates.H,       0],
+    "S":           [braket.circuits.gates.S,       0],
+    "Sdag":        [braket.circuits.gates.Si,      0],
+    "T":           [braket.circuits.gates.T,       0],
+    "Tdag":        [braket.circuits.gates.Ti,      0],
+    "CNOT":        [braket.circuits.gates.CNot,    0],
+    "SWAP":        [braket.circuits.gates.Swap,    0],
+    "CZ":          [braket.circuits.gates.CZ,      0],
+    "X-rotation":  [braket.circuits.gates.Rx,      1],
+    "Y-rotation":  [braket.circuits.gates.Ry,      1],
+    "Z-rotation":  [braket.circuits.gates.Rz,      1],
+    "DenseMatrix": [braket.circuits.gates.Unitary, 2],
+}
+
 class QulacsConverter:
     def __init__(self, circuit: qulacs.QuantumCircuit, convert_type='qiskit'):
         self.convert_type = convert_type
@@ -30,6 +50,7 @@ class QulacsConverter:
 
         convert_dict = {
                 'qiskit': [qiskit_dict, self.qiskit_convert],
+                'braket': [braket_dict, self.braket_convert],
         }
         self.dict = convert_dict[self.convert_type][0]
         self.func = convert_dict[self.convert_type][1]
@@ -56,3 +77,27 @@ class QulacsConverter:
                 qiskit_circuit.append(qiskit_gate(angle), control + target, [])
 
         return qiskit_circuit
+
+    def braket_convert(self):
+        braket_circuit = braket.circuits.Circuit()
+        for i in range(self.circuit.get_gate_count()):
+            gate = self.circuit.get_gate(i)
+            parse = self.dict.get(gate.get_name())
+            if parse is None:
+                parse = self.dict.get("DenseMatrix")
+            braket_gate = parse[0]
+            target = gate.get_target_index_list()
+            control = gate.get_control_index_list()
+            if parse[1] == 0:
+                instr = braket.circuits.Instruction(braket_gate(), control+target)
+                braket_circuit.add(instr)
+            elif parse[1] == 1:
+                angle = gate.get_angle()
+                instr = braket.circuits.Instruction(braket_gate(angle), target)
+                braket_circuit.add(instr)
+            elif parse[1] == 2:
+                matrix=gate.get_matrix()
+                instr = braket.circuits.Instruction(braket_gate(matrix=matrix, display_name=gate.get_name()), target)
+                braket_circuit.add(instr)
+
+        return braket_circuit
